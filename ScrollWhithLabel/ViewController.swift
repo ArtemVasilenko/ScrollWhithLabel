db15137caa09f2b24d814ce81b0459e8bd140075
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-02-18 19:55:17 +0200

    1

--- ScrollWhithLabel/ViewController.swift
+++ ScrollWhithLabel/ViewController.swift
@@ -1,10 +1,4 @@
-//
-//  ViewController.swift
-//  ScrollWhithLabel
-//
-//  Created by Артем on 2/18/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
+
 
 import UIKit
 
@@ -12,7 +6,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
